f6e831b015517bf18b20352ecdfd1c061e7bad32
BlueKNIGHT107 <79380343+BlueKNIGHT107@users.noreply.github.com>
2021-02-22 14:51:32 +0530

    Update wsgi.py

--- Object-Seeker/wsgi.py
+++ Object-Seeker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackathon.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Object-Seeker.settings")
 
 application = get_wsgi_application()
